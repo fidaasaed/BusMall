9fcd1c9bb5bf2699e66e9b72354a10e1c6b3b865
fidaasaed <fidaa.saed97@gmail.com>
2021-03-29 08:25:56 +0300

    branch one solved

---
+++ app.js
@@ -0,0 +1,5 @@
+'use srict'
+var rightSideImageElement = document.getElementById('rightimg');
+var centerSideImageElement = document.getElementById('centerimg');
+var leftSideImageElement = document.getElementById('leftimg');
+var imgContainer = document.getElementById('imgcontainer');
